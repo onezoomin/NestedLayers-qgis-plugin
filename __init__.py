d36448dac75fd993501a83f93849bc2fbd923ad0
gotjosh <gitlab@begreen.nu>
2017-08-16 15:37:49 +0100

    Initial Adaptation

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 # encoding: utf-8
 #-----------------------------------------------------------
-# Copyright (C) 2015 Martin Dobias
+# Forked from NestedLayers (C) 2015 Martin Dobias (GNU GPL 2)
+#-----------------------------------------------------------
+# NestedLayers (C) 2017 Joshua Gottdenker (GNU GPL 2)
 #-----------------------------------------------------------
 # Licensed under the terms of GNU GPL 2
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -14,10 +16,10 @@ from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
 def classFactory(iface):
-    return MinimalPlugin(iface)
+    return NestedLayers(iface)
 
 
-class MinimalPlugin:
+class NestedLayers:
     def __init__(self, iface):
         self.iface = iface
 
